f83ac5b17bc57cb9b44882d099f3ef3b604a133e
Yraida Guillén Paredes <yguillenparedes@gmail.com>
2021-09-16 14:55:47 -0400

    Random card dealer con botón

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 window.onload = function() {
   //write your code here
+  var valorCarta = random(1, 12);
+  var palo = "♦";
+  var colorPalo = "red";
+
+  /* valores palos 
+    1 = spade 
+    2 = club
+    3 = heart
+    4 = diamond
+  */
+  var nuevoPalo = random(1, 4);
+  if (nuevoPalo == 1 || nuevoPalo == 2) {
+    colorPalo = "black";
+  }
+  if (nuevoPalo == 3 || nuevoPalo == 4) {
+    colorPalo = "red";
+  }
+
+  if (nuevoPalo == 1) palo = "♠";
+  if (nuevoPalo == 2) palo = "♣";
+  if (nuevoPalo == 3) palo = "♥";
+  if (nuevoPalo == 4) palo = "♦";
+
+  var paloSup = document.getElementById("EsqSuperior");
+  paloSup.style.color = colorPalo;
+  paloSup.textContent = palo;
+  var paloInf = document.getElementById("EsqInferior");
+  paloInf.style.color = colorPalo;
+  paloInf.textContent = palo;
+  var nroCarta = document.getElementById("numeroCarta");
+  nroCarta.textContent = valorCarta;
   console.log("Hello Rigo from the console!");
 };
